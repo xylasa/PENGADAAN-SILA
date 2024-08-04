512250b105a409899e2bf136c3a98b9843235ddb
Tegar Aditya <tegaraditya53@gmail.com>
2024-08-04 13:52:04 +0700

    fix: preparing for deployment

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\TA\archive\.venv
+set VIRTUAL_ENV=D:\LEARN\Python\PENGADAAN-SILA\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
